d4126d8ea519a1e6c4baa3dcfd07d3b8a8727363
Manju KC <kcmanjuthapa@gmail.com>
2019-09-09 14:18:26 -0400

    added onClick handler and its working

--- src/App.js
+++ src/App.js
@@ -29,12 +29,12 @@ const [valueTigers,setValueTigers] = useState(0);
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => setValueLions(valueLions + 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setValueLions(valueLions + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setValueTigers(valueTigers + 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => setValueTigers(valueTigers + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
